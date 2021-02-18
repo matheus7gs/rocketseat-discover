9332adf30f8b096367e6b98dba0dce286c70fe01
Matheus Gomes <gomesmateus2017@gmail.com>
2021-02-18 10:38:11 -0300

    estruturas de repetição

---
+++ JS/Estruturas de repetição/for-of.js
@@ -0,0 +1,11 @@
+/*
+  Estrutura de repetição
+
+    * for...of
+*/
+let name = 'Matheus'
+let names = ['João', 'Paulo', 'Pedro']
+
+for(let name of names) {
+  console.log(name)
+}
---
+++ JS/Estruturas de repetição/for.js
@@ -0,0 +1,11 @@
+/*
+  Estrutura de repetição
+
+    * for
+      * break - para a execução do momento
+      * continue - pula a execução do momento
+*/
+
+for(let i=0; i<=10; i++) {
+  console.log(i)
+}
